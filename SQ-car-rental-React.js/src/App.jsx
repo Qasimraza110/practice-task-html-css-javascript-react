e06fb891754be3f13f97383ff349e79584c2f971
Qasim Raza <sh.qasim.raza110@gmail.com>
2025-10-01 11:26:16 +0500

    Add files via upload

---
+++ SQ-car-rental-React.js/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div className="bg-white text-gray-900">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
